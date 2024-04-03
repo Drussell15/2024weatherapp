8e94fdd5cfd5b7c1ea36491431a204ed67ccd73e
Drussell15 <russell_dale@hotmail.com>
2024-04-03 18:17:12 -0400

    bracket issue created importexport issue has been fixed

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
-
-
+export default function App() {
 
 const [lat, setLat] = useState([]);
 const [long, setLong] = useState([]);
@@ -20,7 +19,4 @@ return (
 
   </div>
 );
-
-
-
-export default App;
+}
