bcf4949ed3cda41661509a1ca73e9e118f296980
Drussell15 <russell_dale@hotmail.com>
2024-04-03 18:34:35 -0400

    added await fetch async and corrected bracket issues

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -12,10 +12,16 @@ useEffect(() => {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
     });
+    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat, long])
 
-  console.log("Latitude is:", lat)
-  console.log("Longitude is:", long)
-}, [lat, long]);
 return (
   <div className="App">
 
