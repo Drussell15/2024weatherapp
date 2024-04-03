0f4f59b306c23579814a48e2294042da1eedfcd5
Drussell15 <russell_dale@hotmail.com>
2024-04-03 18:47:31 -0400

    created weathercard for weatherJs

---
+++ my-weather-app/src/components/weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import { Card } from 'semantic-ui-react'
+
+const CardExampleCard = ({weatherData}) => (
+    <Card>
+        <CardContent>
+            <Card.Header className="header">(weatherData.name)</Card.Header>
+        </CardContent>
+    </Card>
+)
+
+export default CardExampleCard;
